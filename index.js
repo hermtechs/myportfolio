7a6fb95b89d36b99e29447791c713765a473291e
hermtechs <38890522+hermtechs@users.noreply.github.com>
2020-08-04 15:19:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const navBtn = document.querySelector('.nav-toggle');
 const navMenu = document.querySelector('.nav');
 navBtn.addEventListener('click',()=>{ 
+    
     if(navMenu.style.transform === 'translateX(100%)'){
       navMenu.style.transform = 'translateX(0%)'; 
     }
@@ -10,10 +11,24 @@ navBtn.addEventListener('click',()=>{
     }
 })
 
-//closing menu after clickking on llinks
+//closing menu after clicking on llinks
 const navLinks = document.querySelectorAll('.nav__item');
 navLinks.forEach(link =>{
     link.addEventListener('click', ()=>{
         navMenu.style.transform = 'translateX(100%)';
     })
 })
+//changing logo styles onscroll
+window.onscroll = function(){ scrollFunc(); }
+function scrollFunc(){
+    const logo = document.querySelector('.logo-txt');
+    if (document.body.scrollTop>80 || document.documentElement.scrollTop>80){
+        logo.innerText = '<devherbert>'
+        logo.style.fontSize = '1.2rem';
+    }
+    else {
+        logo.innerText = '<devherbert/>';
+        logo.style.fontSize = '1.4rem';
+    }
+    
+}
